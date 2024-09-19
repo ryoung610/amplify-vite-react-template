c48ed07746ad7a5e3e45c1c7e389537487d3d067
ryoung610 <me0610@gmail.com>
2024-09-19 11:11:58 -0400

    add authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import {Authenticator} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 const client = generateClient<Schema>();
 
@@ -13,28 +15,42 @@ function App() {
     });
   }, []);
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
+  }
+
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <main>
+            <h1>My todos</h1>
+            <button onClick={createTodo}>+ new</button>
+            <ul>
+              {todos.map((todo) => (
+                <li 
+                  onClick={() => deleteTodo(todo.id)}
+                  key={todo.id}>{todo.content}
+                </li>
+              ))}
+            </ul>
+            <div>
+              🥳 App successfully hosted. Try creating a new todo.
+              <br />
+              <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+                Review next step of this tutorial.
+              </a>
+            </div>
+          </main>
+        </div>
+      )}
+    </Authenticator>
   );
 }
-
 export default App;
